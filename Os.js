32242a4c669ba09eea4a157db8d405dad3091cb1
Sujon Sheikh <sujonsheikh.in@gmail.com>
2022-09-19 14:22:43 -0700

    os done

---
+++ Os.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+console.log(os.homedir())
+console.log(os.hostname())
+// console.log(os.cpus())
+console.log(os.arch())
+console.log(os.loadavg())
+console.log(os.platform())
+console.log(os.release())
+console.log(os.freemem())
