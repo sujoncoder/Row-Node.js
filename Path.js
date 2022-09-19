af80b70781be9d450feece7dd73f19d413e9b88e
Sujon Sheikh <sujonsheikh.in@gmail.com>
2022-09-19 14:14:25 -0700

    path module done

---
+++ Path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const myPath = 'C:/Users/sujon/Desktop/Row Node/Path.js';
+
+console.log(path.basename(myPath))
+console.log(path.dirname(myPath))
+console.log(path.extname(myPath))
+console.log(path.parse(myPath))
