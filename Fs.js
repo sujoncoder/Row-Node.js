3f50e7159e6fa01dd21855c695acdeec76350e00
Sujon Sheikh <sujonsheikh.in@gmail.com>
2022-09-19 14:54:52 -0700

    fs module

--- Fs.js
+++ Fs.js
@@ -1,2 +1,15 @@
-console.log(__dirname)
-console.log(__filename)
+const fs = require('fs');
+
+// create file and inside some text.
+fs.writeFileSync('myfile.txt', 'Hello programmers')
+
+// Added some same file.
+fs.appendFileSync('myfile.txt', ' I am too lazy')
+
+// Read file.
+fs.readFile('myfile.txt', (error, data) => {
+    console.log(data.toString())
+})
+
+
+console.log('hello world')
